d4a2fec4b0a034e1e0e126b05ea34ecb5aa1826f
xiaoyao9184 <xiaoyao9184@gmail.com>
2015-04-25 09:54:15 +0800

    Adding operating parameters
    1. Run kettle with param;
    2. Repair if nested variable delay;

--- Windows/RUN_J_PARAM_FSREP.bat
+++ Windows/RUN_J_PARAM_FSREP.bat
@@ -1,22 +1,25 @@
 @echo off
 Setlocal enabledelayedexpansion
-::CODER BY xiaoyao9184 1.1
+::CODER BY xiaoyao9184 1.0
 ::TIME 2015-04-08
-::FILE RUN_T_PARAM_NOLOG_SIMP
-::DESC run a transformation with param no log no repositorie
+::FILE RUN_J_PARAM_FSREP
+::DESC run a job with param no log in filesystem repositorie 
 
 
 :v
 
 ::1变量赋值
-set tip=Kettle调度程序：运行含参转换
+set tip=Kettle调度程序：运行资源库含参作业
 set ver=1.0
-set tName=
+set rName=
+set jName=
 set pName=
 set pValue=
 
-set echotName=需要输入Kettle转换文件名
-set esettName=请输入Kettle转换文件名后，然后回车：
+set echorName=需要输入Kettle文件资源库名称
+set esetrName=请输入Kettle文件资源库名称，然后回车：
+set echojName=需要输入Kettle作业名称
+set esetjName=请输入Kettle作业名称，然后回车：
 set echopName=需要输入参数名
 set esetpName=请输入参数名后，然后回车：
 set echopValue=需要输入参数值
@@ -28,7 +31,7 @@ set esetpValue=请输入参数值后，然后回车：
 ::2提示文本
 title %tip% %ver%
 
-echo Kettle调度程序：运行含参转换
+echo Kettle调度程序：运行资源库含参作业
 echo 运行结束后可以关闭
 echo ...
 
@@ -36,9 +39,14 @@ echo ...
 :check
 
 ::3变量检验 参数处理
-if "%tName%"=="" (
-	echo %echotName%
-	set /p tName=%esettName%
+if "%rName%"=="" (
+	echo %echorName%
+	set /p rName=%esetrName%
+)
+
+if "%jName%"=="" (
+	echo %echojName%
+	set /p jName=%esetjName%
 )
 
 if "%pName%"=="" (
@@ -65,12 +73,12 @@ cd data-integration
 
 echo Kettle引擎目录为：%cd%
 echo Kettle工作目录为：%~dp0
-echo Kettle将执行转换：%tName%
-echo Kettle转换参数为：%pName%=%pValue% 
+echo Kettle运行此资源库中的作业：%rName%:%jName%
+echo Kettle作业参数为：%pName%=%pValue% 
 echo 运行中...      Ctrl+C结束程序
 
-::执行Pan
-Pan -file:%~dp0%tName%.ktr "-param:%pName%=%pValue%"
+::执行Kitchen
+kitchen -rep:%rName% -user:admin -pass:admin -job:%jName% "-param:%pName%=%pValue%"
 
 ::执行完毕
 echo 已经执行完毕，可以结束此程序
--- Windows/RUN_T_PARAM_FSREP.bat
+++ Windows/RUN_T_PARAM_FSREP.bat
@@ -1,20 +1,23 @@
 @echo off
 Setlocal enabledelayedexpansion
-::CODER BY xiaoyao9184 1.1
+::CODER BY xiaoyao9184 1.0
 ::TIME 2015-04-08
-::FILE RUN_T_PARAM_NOLOG_SIMP
-::DESC run a transformation with param no log no repositorie
+::FILE RUN_T_PARAM_NOLOG_FSREP
+::DESC run a transformation with param no log in filesystem repositorie 
 
 
 :v
 
 ::1变量赋值
-set tip=Kettle调度程序：运行含参转换
+set tip=Kettle调度程序：运行资源库含参转换
 set ver=1.0
+set rName=
 set tName=
 set pName=
 set pValue=
 
+set echorName=需要输入Kettle文件资源库名称
+set esetrName=请输入Kettle文件资源库名称，然后回车：
 set echotName=需要输入Kettle转换文件名
 set esettName=请输入Kettle转换文件名后，然后回车：
 set echopName=需要输入参数名
@@ -28,7 +31,7 @@ set esetpValue=请输入参数值后，然后回车：
 ::2提示文本
 title %tip% %ver%
 
-echo Kettle调度程序：运行含参转换
+echo Kettle调度程序：运行资源库含参转换
 echo 运行结束后可以关闭
 echo ...
 
@@ -36,6 +39,11 @@ echo ...
 :check
 
 ::3变量检验 参数处理
+if "%rName%"=="" (
+	echo %echorName%
+	set /p rName=%esetrName%
+)
+
 if "%tName%"=="" (
 	echo %echotName%
 	set /p tName=%esettName%
@@ -65,12 +73,12 @@ cd data-integration
 
 echo Kettle引擎目录为：%cd%
 echo Kettle工作目录为：%~dp0
-echo Kettle将执行转换：%tName%
+echo Kettle运行此资源库中的转换：%rName%:%tName%
 echo Kettle转换参数为：%pName%=%pValue% 
 echo 运行中...      Ctrl+C结束程序
 
 ::执行Pan
-Pan -file:%~dp0%tName%.ktr "-param:%pName%=%pValue%"
+Pan -rep:%rName% -user:admin -pass:admin -job:%tName% "-param:%pName%=%pValue%"
 
 ::执行完毕
 echo 已经执行完毕，可以结束此程序
