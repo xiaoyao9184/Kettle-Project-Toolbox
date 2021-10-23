edacc08ba3126bea2e077dc8d03d5fbe4008435a
xiaoyao9184 <xiaoyao9184@gmail.com>
2021-10-23 19:43:01 +0800

    fix script

--- cdc-to-rdb/_test-config/add_connector_in_test.sh
+++ cdc-to-rdb/_test-config/add_connector_in_test.sh
@@ -6,12 +6,12 @@ path=$1
 if [[ ! -f $path ]]
 then
     echo "Run all"
-    for f in $current_path/connector/*.json ; do
+    for f in $current_path/config_of_connector/*.json ; do
         name=$(basename -s .json "$f")
         echo "Run of $name"
         docker run -it --rm --name test_connect_connector_$n \
             --network=test-debezium-distributed_default \
-            --mount "type=bind,source=$current_path/config/$name.json,destination=/connector.json" \
+            --mount "type=bind,source=$current_path/config_of_connector/$name.json,destination=/connector.json" \
             debezium/tooling:latest bash -c \
 		 	    "curl -i -X POST -H 'Accept:application/json' -H 'Content-Type:application/json' -d @/connector.json connects:8083/connectors/"
                 # "curl connects:8083"
