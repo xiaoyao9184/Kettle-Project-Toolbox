d97d7642d0038ef35fafb83d2a8e703e7de59c82
xiaoyao9184 <xiaoyao9184@gmail.com>
2022-04-16 22:18:31 +0800

    support UPDATE_SCRIPT

---
+++ tool/UPDATE_SCRIPT.bat
@@ -0,0 +1,141 @@
+@ECHO OFF
+SETLOCAL EnableDelayedExpansion
+::CODER BY xiaoyao9184 1.0
+::TIME 2022-04-16
+::FILE UPDATE_SCRIPT
+::SYNTAX UPDATE_SCRIPT [target_path [source_path]... ]
+
+:init_variable
+
+::version
+SET tip=Kettle-Project-Toolbox: update script
+SET ver=1.0
+SET NL=^
+
+
+rem two empty line required
+
+::interactive 1 for true
+ECHO %CMDCMDLINE% | FIND /I "%~0" >NUL
+IF NOT ERRORLEVEL 1 ( SET interactive=0 ) ELSE ( SET interactive=1 )
+
+::script info
+SET current_script_dir=%~dp0
+
+::interactive tip
+SET tip_set_target_path=Please input target_path or drag path in:
+SET tip_miss_target_path=Missing param 'target_path' at position 1.
+SET tip_set_source_path=Please input source_path or drag path in:
+SET tip_miss_source_path=Missing param 'source_path' at position 1.
+
+::default param
+SET target_path=%1
+SET source_path=
+
+
+:tip_version
+
+IF %interactive% equ 1 ( TITLE %tip% %ver% ) ELSE ( ECHO %tip% )
+
+
+:check_variable
+
+IF "%target_path%"=="" (
+    IF %interactive% equ 1 ( 
+        SET /p target_path=%tip_set_target_path%
+        GOTO:check_variable
+    ) ELSE ( 
+        ECHO %tip_miss_target_path%
+        EXIT /B 1
+    )
+) ELSE (
+    SHIFT
+)
+FOR %%F IN (%target_path%.) DO SET target_path=%%~dpnF
+
+:loop__source_path
+    IF "%1"=="" GOTO:end__source_path
+    IF "!source_path!"=="" (
+        SET source_path=%1
+    ) ELSE (
+        SET source_path=!source_path!!NL!%1
+    )
+    SHIFT
+    GOTO:loop__source_path
+:end__source_path
+
+IF "!source_path!"=="" (
+    IF %interactive% equ 1 ( 
+        SET /p source_path=%tip_set_source_path%
+        GOTO:check_variable
+    ) ELSE ( 
+        ECHO %tip_miss_source_path%
+        EXIT /B 1
+    )
+)
+
+
+:begin
+
+::print info for debug
+ECHO ===========================================================
+ECHO Work directory is: %current_script_dir%
+ECHO Target directory is: %target_path%
+ECHO Source directory is: 
+ECHO !source_path!
+ECHO ===========================================================
+ECHO Running...      Ctrl+C for exit
+
+::change to script directory
+CD %current_script_dir%
+
+::bat
+FOR %%f IN (%target_path%\*.bat) DO (
+    SET target_file_path=%%f
+    FOR /F "tokens=1,2 delims= " %%A IN (!target_file_path!) DO (
+        IF "%%A"=="::FILE" (
+            SET source_file_name=%%B.bat
+            FOR /F "delims=" %%S IN ("!source_path!") DO (
+                SET source_dir=%%S
+                SET source_file_path=!source_dir!\!source_file_name!
+                IF EXIST !source_file_path! (
+                    ECHO !target_file_path! ^<- !source_file_path!
+                    COPY /Y "!source_file_path!" "!target_file_path!"
+                )
+            )
+        )
+    )
+)
+
+::sh
+FOR %%f IN (%target_path%\*.sh) DO (
+    SET target_file_path=%%f
+    FOR /F "tokens=1,2,3 delims= " %%A IN (!target_file_path!) DO (
+        IF "%%B"=="FILE" (
+            SET source_file_name=%%C.sh
+            FOR /F "delims=" %%S IN ("!source_path!") DO (
+                SET source_dir=%%S
+                SET source_file_path=!source_dir!\!source_file_name!
+                IF EXIST !source_file_path! (
+                    ECHO !target_file_path! ^<- !source_file_path!
+                    COPY /Y "!source_file_path!" "!target_file_path!"
+                )
+            )
+        )
+    )
+)
+
+
+:done
+
+IF %ERRORLEVEL% equ 0 (
+    ECHO Ok, run done!
+) ELSE (
+    ECHO Sorry, some error make failure!
+)
+
+
+:end
+
+IF %interactive% equ 1 PAUSE
+EXIT /b %ERRORLEVEL%
---
+++ tool/UPDATE_SCRIPT.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+# CODER BY xiaoyao9184 1.0
+# TIME 2021-04-16
+# FILE UPDATE_SCRIPT
+# SYNTAX UPDATE_SCRIPT [target_path [source_path]... ]
+
+
+#init_variable
+
+# version
+tip="Kettle-Project-Toolbox: update script"
+ver="1.0"
+
+# here interactive mean user input can be obtained, 
+# determined by checking is connected to a terminal 
+[[ -t 0 || -p /dev/stdin ]] && interactive=1 || interactive=0
+
+# script info
+current_script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# interactive tip
+tip_set_target_path="Please input target_path or drag path in:"
+tip_miss_target_path="Missing param 'target_path' at position 1."
+tip_set_source_path="Please input source_path or drag path in:"
+tip_miss_source_path="Missing param 'source_path' at position 1."
+
+# default param
+target_path=$1
+source_path=
+
+
+#tip_version
+
+[ $interactive -eq 1 ] && echo -e '\033]2;'$tip $ver'\007' || echo "$tip"
+
+
+#check_variable
+
+while [[ -z $target_path ]]
+do
+    if [[ $interactive -eq 1 ]]
+    then 
+        read -p "$tip_set_target_path" target_path
+    else 
+        echo $tip_miss_target_path
+        exit 1
+    fi
+done
+
+shift
+source_path="$@"
+while [[ -z "$source_path" ]]
+do
+    if [[ $interactive -eq 1 ]]
+    then 
+        read -p "$tip_set_source_path" source_path
+    else 
+        echo $tip_miss_source_path
+        exit 1
+    fi
+done
+IFS=' ' read -r -a source_path <<< "$source_path"; unset IFS;
+# source_path=( "$source_path" )
+
+#begin
+
+# print info for debug
+echo "==========================================================="
+echo "Work directory is: $current_script_dir"
+echo "Target directory is: $target_path"
+echo "Source directory is: $source_path"
+echo "==========================================================="
+echo "Running...      Ctrl+C for exit"
+
+# change to script directory
+cd $current_script_dir
+
+# bat
+FILES="$target_path/*.bat"
+for f in $FILES
+do
+    target_file_path="$f"
+    line=$(grep '::FILE' $target_file_path)
+    list=($line)
+    # https://stackoverflow.com/questions/44945089/echo-printing-variables-in-a-completely-wrong-order
+    source_file_name=$(echo "${list[1]}" | tr -d '\r')
+    source_file_name="$source_file_name.bat"
+    for s in "${source_path[@]}"
+    do
+        source_dir="$s"
+        source_file_path="$source_dir/$source_file_name"
+        if [[ -f "$source_file_path" ]]
+        then
+            echo "$target_file_path <- $source_file_path"
+            cp -f $source_file_path $target_file_path
+        fi
+    done
+done
+
+# sh
+FILES="$target_path/*.sh"
+for f in $FILES
+do
+    target_file_path="$f"
+    line=$(grep '# FILE' $target_file_path)
+    list=($line)
+    source_file_name="${list[2]}.sh"
+    for s in "${source_path[@]}"
+    do
+        source_dir="$s"
+        source_file_path="$source_dir/$source_file_name"
+        if [[ -f "$source_file_path" ]]
+        then
+            echo "$target_file_path <- $source_file_path"
+            cp -f $source_file_path $target_file_path
+        fi
+    done
+done
+
+
+#done
+
+code=$?
+if [[ "$code" -eq "0" ]]
+then
+    echo "Ok, run done!"
+else
+    echo "Sorry, some error make failure!"
+fi
+
+
+#end
+
+if [[ $interactive -eq 1 ]]
+then
+    read -p "Press enter to continue"
+    exit $code
+else
+    exit $code
+fi
