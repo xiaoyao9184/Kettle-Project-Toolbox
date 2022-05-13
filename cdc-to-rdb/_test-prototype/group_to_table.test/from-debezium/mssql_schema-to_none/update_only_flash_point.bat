d985f8e13d6d0efc2e3f18b6906decc8d1a5cff4
xiaoyao9184 <xiaoyao9184@gmail.com>
2022-05-13 17:54:34 +0800

    group_to_table.test

---
+++ cdc-to-rdb/_test-prototype/group_to_table.test/from-debezium/mssql_schema-to_none/update_only_flash_point.bat
@@ -0,0 +1,56 @@
+@ECHO OFF
+SETLOCAL EnableDelayedExpansion
+
+@REM like this
+@REM SET KETTLE_HOME=E:/Kettle/workspace9.1/cdc-to-rdb/
+@REM SET KETTLE_REPOSITORY=cdc-to-rdb
+@REM pan /job:_test-prototype/group_to_table.test
+
+
+SET current_script_dir=%~dp0
+
+FOR %%F IN (%current_script_dir%.) DO SET project_folder_path=%%~dpF%%~nF
+:find_project_path_loop
+IF NOT EXIST "%project_folder_path%\db_kpt_cdc_event_pgsql_writer.kdb" (
+    FOR %%F IN (%project_folder_path%.) DO SET project_folder_path=%%~dpF%
+    ECHO go up !project_folder_path!
+    GOTO:find_project_path_loop
+)
+
+
+SET KPT_COMMAND=pan
+SET KPT_PARAM_AS_ENV=true
+SET KPT_KETTLE_TRANS=_test-prototype/group_to_table.test
+@REM SET KPT_KETTLE_LEVEL=Rowlevel
+
+
+SET KPT_KETTLE_PARAM_Config_Batch_Group_Logger_Mapping=log_json_to_kettle
+SET KPT_KETTLE_PARAM_Config_Delay_Injection_Crud_Time=0
+SET KPT_KETTLE_PARAM_Config_Delay_Injection_Field_Time=0
+SET KPT_KETTLE_PARAM_Config_Log_Source_Column_Mapping=mssql_schema
+SET KPT_KETTLE_PARAM_Config_Log_Source_Key_Mapping=debezium_key_to_key_meta
+SET KPT_KETTLE_PARAM_Config_Log_Source_Transformation_Path=from-debezium
+SET KPT_KETTLE_PARAM_Config_Log_Target_Column_Mapping=same_source
+SET KPT_KETTLE_PARAM_Config_Log_Target_Operate_Mapping=update_only_flash_point
+SET KPT_KETTLE_PARAM_Config_Log_Target_Table_Mapping=same_source_lookup
+SET KPT_KETTLE_PARAM_Config_Log_Target_Transformation_Path=to_none
+SET KPT_KETTLE_PARAM_Param_Group_ID=test
+SET KPT_KETTLE_PARAM_Param_Group_Name=debezium-mssql-operate-update
+SET KPT_KETTLE_PARAM_ParamInjectionPath=_test-prototype/message_convert_to_rdb_rud.injection
+SET KPT_KETTLE_PARAM_ParamMarkTransformation=no_marker
+SET KPT_KETTLE_PARAM_ParamTemplatePath=to_rdb
+SET KPT_KETTLE_PARAM_ParamTemplateTransformation=message_convert_to_rdb_rud.template
+
+@REM for protptype
+SET KPT_KETTLE_PARAM_Config_Log_UpdateOnly_FlashPoint_Include=INSERT,SNAPSHOT
+SET KPT_KETTLE_PARAM_Config_Log_UpdateOnly_FlashPoint_Delay=86400
+SET KPT_KETTLE_PARAM_Config_Time=2022-01-01T00:00:00.000Z
+
+
+CALL %project_folder_path%KPT_RUN_COMMAND.bat
+ECHO exit code will be 0
+
+
+ECHO:
+ECHO pause by %~nx0
+PAUSE
