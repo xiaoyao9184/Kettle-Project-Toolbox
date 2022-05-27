5cffdfbf7ae3e57df71632bd498853300bb33053
xiaoyao9184 <xiaoyao9184@gmail.com>
2022-05-27 18:01:59 +0800

    add print-configs in default/_test-entry/

---
+++ default/_test-entry/print-configs.bat
@@ -0,0 +1,23 @@
+@ECHO OFF
+SETLOCAL EnableDelayedExpansion
+
+@REM like this
+@REM SET KETTLE_HOME=E:/Kettle/workspace7.1/default/
+@REM SET KETTLE_REPOSITORY=default
+@REM pan /trans:_test-entry/print-configs
+
+
+SET current_script_dir=%~dp0
+FOR %%F IN (%current_script_dir%.) DO SET parent_folder_dir=%%~dpF
+
+
+SET KPT_COMMAND=pan
+SET KPT_KETTLE_TRANS=_test-entry/print-configs
+
+CALL %parent_folder_dir%KPT_RUN_COMMAND.bat
+
+
+
+ECHO:
+ECHO pause by %~nx0
+PAUSE
