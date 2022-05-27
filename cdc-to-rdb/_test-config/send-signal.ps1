9a5b7d016e35ec0c143373823cfdadfba1dd4787
xiaoyao9184 <xiaoyao9184@gmail.com>
2022-05-27 16:48:28 +0800

    support mysql read.only signal

--- cdc-to-rdb/_test-config/send-signal.ps1
+++ cdc-to-rdb/_test-config/send-signal.ps1
@@ -1,20 +1,60 @@
+function Send-Kafka {
+    [CmdletBinding()]
+    param (
+        [ValidateNotNullOrEmpty()]
+        [string[]]$Name,
+        [string]$FilePath
+    )
+    process {
+        $_name = $Name -join "__"
+
+        $_json = Get-Content "$FilePath" | ConvertFrom-Json 
+        $_network = $_json.network | ForEach-Object {
+            return "--network=$_"
+        }
+        $_bootstrap = $_json.bootstrap
+        $_topic = $_json.topic
+        $_file_parent = Split-Path -Parent $FilePath
+        $_file_name = Split-Path -LeafBase $FilePath
+        $_file = Join-Path -Path $_file_parent -ChildPath ($_file_name + '.csv')
+
+        docker run -it --rm --name $_name `
+            $_network `
+            --mount "type=bind,source=$_file,destination=/send.csv" `
+            --entrypoint sh `
+            edenhill/kcat:1.7.1 `
+                -c "kcat -b $_bootstrap -t $_topic -P -K, < /send.csv"
+
+    }
+}
+
 function Send-Mysql {
     [CmdletBinding()]
     param (
         [ValidateNotNullOrEmpty()]
         [string[]]$Name,
-        [string]$SqlPath
+        [string]$FilePath
     )
     process {
         $_name = $Name -join "__"
-        $_network = "--network=test-kpt"
+
+        $_json = Get-Content "$FilePath" | ConvertFrom-Json 
+        $_network = $_json.network | ForEach-Object {
+            return "--network=$_"
+        }
+        $_host = $_json.host
+        $_user = $_json.user
+        $_password = $_json.password
+        $_file_parent = Split-Path -Parent $FilePath
+        $_file_name = Split-Path -LeafBase $FilePath
+        $_file = Join-Path -Path $_file_parent -ChildPath ($_file_name + '.sql')
 
         docker run -it --rm --name $_name `
             $_network `
-            --mount "type=bind,source=$SqlPath,destination=/run.sql" `
+            --mount "type=bind,source=$_file,destination=/send.sql" `
             --entrypoint /bin/bash `
             mysql:5.7 `
-            -c "mysql -hmysql -uroot -pP@ssw0rd < /run.sql"
+                -c "mysql -h$_host -u$_user -p$_password < /send.sql"
     }
 }
 
@@ -23,16 +63,17 @@ function Send-Sql {
     param (
         [ValidateNotNullOrEmpty()]
         [string[]]$Name,
-        [string]$SqlPath
+        [string]$FilePath
     )
     process {
 
-        $parent_path = Split-Path -Parent $SqlPath
+        $parent_path = Split-Path -Parent $FilePath
         $db_platform = Split-Path -LeafBase $parent_path
         
         Switch ($db_platform)
         {
-            "mysql" { Send-Mysql ($Name) $SqlPath }
+            "mysql" { Send-Mysql ($Name) $FilePath }
+            "kafka" { Send-Kafka ($Name) $FilePath }
             default { Write-Host "Not support database platform" }
         }
 
@@ -49,9 +90,9 @@ function Send-Workspace {
     process {
         Write-Host "Run $Name in $Workspace"
 
-        $sub_paths = Get-ChildItem -Path "$Workspace\*" -Include *.sql 
+        $sub_paths = Get-ChildItem -Path "$Workspace\*" -Include *.json 
         foreach ($sub_path in $sub_paths){
-            Write-Host "process sql $sub_path"
+            Write-Host "process json $sub_path"
             Send-Sql ($name) $sub_path
         }
     }
