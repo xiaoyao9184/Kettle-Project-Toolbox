75325b60c939244ae32f80367882f06dd887082d
xiaoyao9184 <xiaoyao9184@gmail.com>
2022-07-11 11:21:24 +0800

    support ParamMarkPath

--- cdc-to-rdb/_test-prototype/cdc_to_batch.test/test_debezium_mysql-test_kpt_cdc-json.data-changes.bat
+++ cdc-to-rdb/_test-prototype/cdc_to_batch.test/test_debezium_mysql-test_kpt_cdc-json.data-changes.bat
@@ -29,7 +29,7 @@ SET _datetime=!_date!-!_time!
 SET KPT_COMMAND=pan
 SET KPT_PARAM_AS_ENV=true
 SET KPT_KETTLE_TRANS=_test-prototype/cdc_to_batch.prototype
-
+@REM cdc_to_batch.prototype param
 SET KPT_KETTLE_PARAM_Config_CDC_Kafka_Batch_Duration=10000
 SET KPT_KETTLE_PARAM_Config_CDC_Kafka_Batch_Max=100
 SET KPT_KETTLE_PARAM_Config_CDC_Kafka_Batch_Size=100
@@ -38,7 +38,12 @@ SET KPT_KETTLE_PARAM_Config_CDC_Kafka_Consumer_Group=cdc_to_batch.test-%_datetim
 SET KPT_KETTLE_PARAM_Config_CDC_Kafka_Data_Topic=test_debezium_mysql-test_kpt_cdc-json.data-changes
 SET KPT_KETTLE_PARAM_ParamBatchPath=/_test-prototype
 SET KPT_KETTLE_PARAM_ParamBatchTransformation=batch_to_file.stream
+@REM batch_to_file.stream param
 SET KPT_KETTLE_PARAM_ParamCsvPath=_test-prototype/cdc_to_batch.test
+SET KPT_KETTLE_PARAM_ParamMarkPath=/_test-prototype
+SET KPT_KETTLE_PARAM_ParamMarkTransformation=debug_vars_marker.result
+@REM debug_vars_marker.result param
+SET KPT_KETTLE_PARAM_ParamDebug=true
 
 SET KPT_CALLER_SCRIPT_PATH=%project_folder_path%test.cdc_to_batch.%KPT_KETTLE_PARAM_Config_CDC_Kafka_Data_Topic%.bat
 CALL %project_folder_path%KPT_RUN_COMMAND.bat
